2bbceec7fbeb51fa12dc8aa923ea43d534758fa9
HenryHall <mnboogie@yahoo.com>
2016-07-19 09:50:13 -0500

    first commit

---
+++ server/app.js
@@ -0,0 +1,12 @@
+var express=require('express');
+var app=express();
+var path=require('path');
+var bodyParser=require('body-parser');
+var urlencodedParser=bodyParser.urlencoded( { extended: false } );
+
+app.use( express.static( 'public' ) );
+
+app.get( '/', function( req, res ){
+  console.log( 'at base url' );
+  res.sendFile( path.resolve( 'views/index.html' ) );
+}); // end base url
