8b180447b28532c9cce6aa937ba5fc40985dd5f2
mablchnole <mablchnole@gmail.com>
2016-07-22 12:35:18 -0500

    ng routes are functional

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,1 +1,16 @@
-console.log("yo");
+var myApp = angular.module('myApp', [
+  'ngRoute'
+]);
+
+myApp.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.
+    when('/home', {
+      templateUrl: '/views/home.html',
+    }).
+    when('/inquiry', {
+      templateUrl: '/views/inquiry.html'
+    }).
+    otherwise({
+    redirectTo: 'home'
+  });
+}]);
