6384db807b9bb928569a5d8a3a19515d41c4f15c
ngorbunov <nick_g@outlook.com>
2016-07-22 13:50:05 -0500

    working registration, not login

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,1 +1,19 @@
-console.log("yo");
+var myApp = angular.module('myApp', []);
+
+
+myApp.controller('loginController', ['$scope', '$http', function( $scope , $http){
+
+  $scope.register = function(){
+    var loginObject = {
+      email: $scope.email,
+      password: $scope.password
+    };
+
+    $http({
+      method: 'POST',
+      url: '/register',
+      data: loginObject
+    });
+  };
+
+}]);
--- server/app.js
+++ server/app.js
@@ -2,6 +2,9 @@ var express=require('express');
 var app=express();
 var path=require('path');
 var bodyParser=require('body-parser');
+var pg = require('pg');
+var passport = require('../server/strategies/userStrategy.js');
+var session = require('express-session');
 var urlencodedParser=bodyParser.urlencoded( { extended: false } );
 
 app.use( express.static( 'public' ) );
@@ -17,3 +20,28 @@ app.set('port', process.env.PORT || 4200);
 app.listen(app.get('port'), function() {
   console.log('human, wake up:', app.get('port'));
 });
+
+//body paser
+app.use(bodyParser.json());
+app.use(express.static('public'));
+
+// Passport Session Configuration //
+app.use(session({
+   secret: 'secret',
+   key: 'user',
+   resave: 'true',
+   saveUninitialized: false,
+   cookie: {maxage: 60000, secure: false}
+}));
+
+// init passport
+app.use(passport.initialize());
+app.use(passport.session());
+
+//Include Routes
+var index = require('../server/routes/indexRoute');
+var register = require('../server/routes/registerRoute');
+
+// Routes
+app.use('/register', register);
+app.use('/*', index);
