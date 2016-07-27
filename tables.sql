29a36757d986d2f42ce3a7b0cc20bb3d480edc57
Bethany Johnson <johnson.l.bethany@gmail.com>
2016-07-27 15:05:19 -0500

    Update tables, add ENUM

--- tables.sql
+++ tables.sql
@@ -1,22 +1,26 @@
+CREATE TYPE status AS ENUM ('New Inquiry', 'Pending Inquiry', 'Approved Inquiry', 'New Application', 'Pending Application', 'Approved Application', 'Admin');
+CREATE TYPE role AS ENUM ('K9 Handler', 'K9 Unit Supervisor', 'Department Admin', 'Other Admin Staff', 'Other Command Staff');
+
+-- this table takes in data from either basic or full form. not every user will be associated with a k9/handler, when not auth signer --
 CREATE TABLE users (
 	id SERIAL PRIMARY KEY NOT NULL,
 	email VARCHAR(255) UNIQUE NOT NULL,
 	contact_email VARCHAR(255) UNIQUE NOT NULL,
   	password VARCHAR(255) UNIQUE NOT NULL,
-  	status VARCHAR(255) DEFAULT 'New Inquiry' NOT NULL,
+  	status STATUS DEFAULT 'New Inquiry' NOT NULL,
 	created TIMESTAMP DEFAULT current_timestamp NOT NULL,
 	rank VARCHAR(255),
-	role VARCHAR(255),
+	role ROLE,
 	first_name VARCHAR(255) NOT NULL,
 	last_name VARCHAR(255) NOT NULL,
 	primary_phone VARCHAR(30) NOT NULL,
 	alt_phone VARCHAR(30),
 	contact_time VARCHAR(30) NOT NULL,
-	add_street1 VARCHAR(255),
-	add_street2 VARCHAR(255),
-	add_city VARCHAR(255),
-	add_state VARCHAR(30),
-	add_zip INTEGER,
+	dept_add_street1 VARCHAR(255),
+	dept_add_street2 VARCHAR(255),
+	dept_add_city VARCHAR(255),
+	dept_add_state VARCHAR(30),
+	dept_add_zip INTEGER,
 	dept_k9s INTEGER,
 	auth_title VARCHAR(255),
 	auth_first_name VARCHAR(255),
@@ -27,7 +31,7 @@ CREATE TABLE users (
 
 CREATE TABLE K9s (
 	id SERIAL PRIMARY KEY NOT NULL,
-	user_id SERIAL UNIQUE NOT NULL,
+	user_id INTEGER NOT NULL REFERENCES users(id),
 	k9_name VARCHAR(255) NOT NULL,
 	breed VARCHAR(255) NOT NULL,
 	age VARCHAR(30) NOT NULL,
@@ -41,7 +45,8 @@ CREATE TABLE K9s (
 	handler_secondary_phone VARCHAR(30),
 	handler_email VARCHAR(255) NOT NULL,
 	signed TIMESTAMP DEFAULT current_timestamp NOT NULL,
-	k9_bio TEXT NOT NULL,
+-- user will have ability to save the below data and return later to complete form, thus made nullable --
+	k9_bio TEXT,
 	k9_back INTEGER,
 	k9_chest INTEGER,
 	k9_girth INTEGER,
@@ -54,7 +59,7 @@ CREATE TABLE K9s (
 	squad_retirement BOOLEAN
 );
 
-CREATE TABLE certification_types (
+CREATE TABLE certifications (
 	id SERIAL PRIMARY KEY NOT NULL,
 	name VARCHAR(255) NOT NULL,
 	url VARCHAR(1000) NOT NULL
@@ -68,23 +73,23 @@ CREATE TABLE equipment (
 CREATE TABLE K9_photos (
 	id SERIAL PRIMARY KEY NOT NULL,
 	url VARCHAR(1000) NOT NULL,
-	K9_id INTEGER NOT NULL
+	K9_id INTEGER NOT NULL REFERENCES K9s(id)
 );
 
 CREATE TABLE squad_photos (
 	id SERIAL PRIMARY KEY NOT NULL,
 	url VARCHAR(1000) NOT NULL,
-	K9_id INTEGER NOT NULL
+	K9_id INTEGER NOT NULL REFERENCES K9s(id)
 );
 
--- joins tables --
-CREATE TABLE K9_certifications (
+-- junction tables for many to many joins --
+CREATE TABLE K9s_certifications (
 	id SERIAL PRIMARY KEY NOT NULL,
 	K9_id INTEGER NOT NULL,
-	certification_id INTEGER NOT NULL REFERENCES certification_types(id)
+	certification_id INTEGER NOT NULL REFERENCES certifications(id)
 );
 
-CREATE TABLE K9_equipment (
+CREATE TABLE K9s_equipment (
 	id SERIAL PRIMARY KEY NOT NULL,
 	K9_id INTEGER NOT NULL REFERENCES K9s(id),
 	equipment_id INTEGER NOT NULL REFERENCES equipment(id)
