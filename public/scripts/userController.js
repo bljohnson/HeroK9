a10bcc16bc6d52eaca3a38429d5f0a4b34b35d67
mablchnole <mablchnole@gmail.com>
2016-07-29 16:45:35 -0500

    add form fields

--- public/scripts/userController.js
+++ public/scripts/userController.js
@@ -10,6 +10,54 @@ angular.module('myApp').controller('UserController', [
     $scope.username = "Officer Henry Hall";
 
     ////////////////////////////////////////////////////////////
+    //                  FORM FUNCTIONALITY                    //
+    ////////////////////////////////////////////////////////////
+
+    // certification checkboxes
+    $scope.items = ['Explosives', 'Narcotics', 'Patrol', 'Trailing', 'Tracking', 'Other'];
+    $scope.selected = [1];
+
+    $scope.toggle = function (item, list) {
+      var idx = list.indexOf(item);
+      if (idx > -1) {
+        list.splice(idx, 1);
+      }
+      else {
+        list.push(item);
+      }
+    };
+    $scope.exists = function (item, list) {
+      return list.indexOf(item) > -1;
+    };
+    $scope.isIndeterminate = function() {
+      return ($scope.selected.length !== 0 &&
+          $scope.selected.length !== $scope.items.length);
+    };
+    $scope.isChecked = function() {
+      return $scope.selected.length === $scope.items.length;
+    };
+    $scope.toggleAll = function() {
+      if ($scope.selected.length === $scope.items.length) {
+        $scope.selected = [];
+      } else if ($scope.selected.length === 0 || $scope.selected.length > 0) {
+        $scope.selected = $scope.items.slice(0);
+      }
+    };
+
+    // select vest colors
+    $scope.colors = ['Black', 'Tan', 'Green'];
+    $scope.getVestColor = function() {
+      if ($scope.color !== undefined) {
+        return $scope.color;
+      } else {
+        return "Please select a color";
+      }
+    };
+
+
+
+
+    ////////////////////////////////////////////////////////////
     //                      FILE UPLOADS                      //
     ////////////////////////////////////////////////////////////
 
@@ -26,7 +74,7 @@ angular.module('myApp').controller('UserController', [
       }
     };
 
-    // upload files to S3
+    // upload files to S3 and to the database
     $scope.upload = function(file) {
       Upload.upload ({
         url: '/userDash/uploads',
@@ -37,6 +85,17 @@ angular.module('myApp').controller('UserController', [
         }
       }).then(function(resp) {
         console.log('Success ' + resp.config.data.file.name + ' uploaded. Response: ' + resp.data);
+
+        // then, if success, also send data and file location to database
+
+        // collect input and create object to send
+
+        // post method to send inputs to database
+
+        // redirect users to their dashboard after form submission
+        // $location.path('/');
+
+
       }, function(resp) {
         console.log('Error status: ' + resp.status);
       }, function(evt) {
