cb5cdd717b7931fb331ea3e1d10609d0f4b54363
HenryHall <mnboogie@yahoo.com>
2016-08-01 10:46:57 -0500

    pulled files

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -282,6 +282,7 @@ myApp.controller('AppController', ['$scope', '$http', function($scope, $http){
      handlerTitle: $scope.title,
      handlerFirstName: $scope.first,
      handlerLastName: $scope.last,
+     handlerBadge: $scope.badge,
      handlerCellPhone: $scope.cell,
      handlerSecondaryCell: $scope.secondaryCell,
      handlerEmail: $scope.emailAddress,
