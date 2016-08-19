8837326847cc152d2115086e58b7160fe9c5d482
HenryHall <mnboogie@yahoo.com>
2016-08-19 11:43:13 -0500

    database host

--- server/modules/connection.js
+++ server/modules/connection.js
@@ -1,3 +1,15 @@
-var connectionString = 'postgres://localhost:5432/HeroK9';
+
+var pg = require('pg');
+var connectionString = '';
+if(process.env.DATABASE_URL !== undefined) {
+ console.log('env connection string');
+ connectionString = process.env.DATABASE_URL;
+ pg.defaults.ssl = true;
+} else {
+ connectionString = 'postgres://localhost:5432/HeroK9';
+}
+console.log("connectionString set to: ", connectionString);
+
+
 
 module.exports = connectionString;
